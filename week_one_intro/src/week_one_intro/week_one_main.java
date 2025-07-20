ce947002084a975b7768668cea04afe37b89f083
chrisflb <christian.flores1012@gmail.com>
2025-07-20 15:50:46 -0700

    "added fictional character variables"

--- week_one_intro/src/week_one_intro/week_one_main.java
+++ week_one_intro/src/week_one_intro/week_one_main.java
@@ -3,8 +3,17 @@ package week_one_intro;
 public class week_one_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String first_name = "Manny";
+		String last_name = "Parker";
+		String street_adress ="1012 Broke Dr.";
+		String city = "Summerville";
+		String zip_code = "1080";
+		
+		System.out.println(first_name);
+		System.out.println(last_name);
+		System.out.println(street_adress);
+		System.out.println(city);
+		System.out.println(zip_code);
 	}
 
 }
